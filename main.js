259f81e913f8985491f923f5644841f3f6d2bbe9
jakedub <jacob.w.moore@gmail.com>
2017-08-09 17:00:52 -0400

    not close

--- main.js
+++ main.js
@@ -19,19 +19,8 @@ let website = [];
         response.json().then(function(data) { //data is present in array
           for (let i=0; i<data.length; i++)
           receivedData.push(data[i]);
+          responsdedData(data);
           console.log(receivedData);
-
-          for (let j=0; j<receivedData.length; j++)
-          name.push(receivedData[j].name)
-          console.log(name);
-
-          for (let n=0; n<receivedData.length; n++)
-          identifier.push(receivedData[n].id)
-          console.log(identifier);
-
-          for (let x=0; x<receivedData.length; x++)
-          website.push(receivedData[x].website)
-          console.log(website);
         });
       }
     )
@@ -39,15 +28,8 @@ let website = [];
       console.log("Fetch Error :-S", err);
     });
 
-// function searchFunction (){
-//   let inputValue = "";
-//   if (inputValue === receivedData.text){
-//     return name;
-//   } else if (something being entered === website from identifier) {
-//     return name;
-//   } else if (something being entered === name from identifier){
-//     return name;
-//   } else {
-//     return "Something went wrong";
-//   }
+
+// function replace(){
+//   let inputValue = input.value;
+//   let source = "http://jsonplaceholder.typicode.com/users"
 // }
