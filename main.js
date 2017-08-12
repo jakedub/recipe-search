f1cab7cbd058be94cec4020d17d0a7bd3fdb620c
Jacob <jacob.w.moore@gmail.com>
2017-08-12 14:21:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,3 @@
-// //Element Grabs
-// let inputElement = document.getElementById("myInput");
-// let container = document.getElementsByClassName("container")
-// let api= "http://jsonplaceholder.typicode.com/users?="
-//
-// function fetchStuff(){
-// fetch(api)
-//     .then(function(response) {
-//         if (response.status !== 200) {
-//           return;
-//         }
-//         response.json().then(function(Object) {
-//           //data is present in array
-//           console.log(Object);
-//
-//           let inputValue=inputElement.value;
-//           // console.log(inputValue);
-//           for (i in Object){
-//               console.log("I am here");
-//             if(inputValue===Object[i].name){
-//               console.log(inputValue===Object[i].name);
-//             }
-//           }
-//         });
-//       }
-//     )
-//     .catch(function(err) {
-//       console.log("Fetch Error :-S", err);
-//     });
-// }
-//
-// inputElement.addEventListener("keypress", itemKeyPress);
-// function itemKeyPress(event){
-//   if (event.key == 'Enter'){
-//     fetchStuff();
-//     inputElement.value = "";
-//   }
-// }
-
-
-
 let container = document.querySelector(".container")
 let inputElement = document.getElementById("myInput");
 
@@ -65,12 +24,12 @@ function fetchStuff() {
               if (input2===data[i].name){
                 console.log(data[i].name);
                 returnResponse.innerHTML = `
-                <p></p>
-                <img src="http://via.placeholder.com/100" alt="Image Missing">
+                <div class="box">
                 <p><span>Name:</span>${data[i].name}</p>
                 <p><span>ID: </span>${data[i].id}</p>
                 <p><span>Username: </span>${data[i].username}</p>
                 <p><span>Email: </span>${data[i].email}</p>
+                </div>
                 `;
                 container.appendChild(returnResponse);
             }}
