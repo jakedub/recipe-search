134be00d6d059a971a9093616e13682e0558c3ce
Jacob <jacob.w.moore@gmail.com>
2017-08-29 14:19:11 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
-let container = document.querySelector(".container")
-let inputElement = document.getElementById("myInput");
+/*
+  Here is a rough idea for the steps you could take:
+*/
 
-//set event listener to trigger search only after 'enter' is pressed.
-inputElement.addEventListener("keypress", function(event) {
+// 1. First select and store the elements you'll be working with
+let player = document.getElementsByClassName("player");
+let music_player = document.getElementsByClassName("music-player");
+let search = document.getElementsByClassName("search");
+let search_form = document.getElementsByClassName("search-form");
+let results = document.querySelector(".results");
+let inputElement = document.getElementById("myInput");
+let searchInput = document.getElementById("search_button");
+let music = document.getElementById("music");
+let preview = [];
+// 2. Create your `submit` event for getting the user's search term
+
+//Event Listener for the KeyPress
+
+inputElement.addEventListener("keypress", function(event) { // NOTE: Need to change the event listener to Submit button click
   if (event.key == "Enter") {
-    fetchStuff();
+    event.preventDefault();
+    dataPull(inputElement.value );
   }
 });
 
-//fetch
-function fetchStuff() {
-  fetch("http://jsonplaceholder.typicode.com/users?=")
+searchInput.addEventListener("click", dataPull);
+
+// 3. Create your `fetch` request that is called after a submission.
+
+function dataPull(search) {
+  fetch("https://itunes.apple.com/search?limit=15&term=" + search)
       .then(function(response) {
           if (response.status !== 200) {
+            console.log(response.status);
             return;
           }
 
          //successful fetch function
           response.json().then(function(data){
-            let returnResponse = document.createElement("div");
-            let input2 = inputElement.value;
-            for (let i=0; i<data.length; i++){
-              if (input2===data[i].name){
-                console.log(data[i].name);
-                returnResponse.innerHTML = `
-                <div class="box">
-                <p><span>Name:</span>${data[i].name}</p>
-                <p><span>ID: </span>${data[i].id}</p>
-                <p><span>Username: </span>${data[i].username}</p>
-                <p><span>Email: </span>${data[i].email}</p>
-                </div>
-                `;
-                container.appendChild(returnResponse);
-            }}
-            console.log(data);
+            attached(data.results);
           });
         }
       )
@@ -42,4 +46,34 @@ function fetchStuff() {
       .catch(function(err) {
         console.log("Fetch Error :-S", err);
       });
+}
+
+
+// 4. Create a way to append the fetch results to your page
+// NOTE: Looks like I'm doing this within the fetch request. Could put this in another function like:
+function attached(songs){ //would need to update 3. and replace the below with a call of the function attached like: attached(pulledData) where pulledData is the data.results
+preview = [];
+  results.innerHTML = "";
+  for (let i=0; i<songs.length; i++){
+      let returnResponse = document.createElement("div");
+      returnResponse.setAttribute('onclick', `play(${i})`);
+      returnResponse.innerHTML = `
+      <div class="box">
+      <img src=${songs[i].artworkUrl60} alt="Album Image">
+      <div class="interior">
+      ${songs[i].trackName} <br/>
+      ${songs[i].artistName}
+      </div>
+      </div>
+      `;
+      results.appendChild(returnResponse);
+      preview.push(songs[i].previewUrl);
+}
+}
+// 5. Create a way to listen for a click that will play the song in the audio play
+
+function play(i){
+  console.log(preview[i]);
+  audio = document.getElementById("music");
+  music.setAttribute('src', preview[i]);
 }
